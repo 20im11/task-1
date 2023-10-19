0f4f23f354da440d75db29cf1cf238b0288a3536
20im11 <imariamidze11@gmail.com>
2023-10-19 14:43:58 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// task1
+
+
+let number = 100;
+if (number < 50) {
+    console.log("<50");
+} else if (number > 20) {
+    console.log(">20");
+} else {
+    console.log("error")
+}
+
+
+
+// task2
+
+let userName1 = "Mariam";
+let result = (userName1 == "Mariam") ? "true" : "false";
+console.log(result);
+
+
+
+// task3
+let userName2 = "Mariam";
+switch(userName2) {
+    case "Mariam":
+        console.log("true");
+        break;
+        default:console.log("false");
+}
+
+
+    
